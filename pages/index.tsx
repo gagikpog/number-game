0048c696953447b1bf1350b3d796ed26e3616021
Gagik <gagikpog@gmail.com>
2022-10-21 14:09:33 +0500

    add mete tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,17 @@ import { theme } from '../common/themes';
 import Main from '../common/components/main';
 import { Provider } from 'react-redux';
 import { store } from '../store/main';
+// @ts-ignore
+import MetaTags from 'react-meta-tags';
 
 const Index: NextPage = () => {
     return (
         <ThemeProvider theme={theme}>
             <Provider store={store}>
+                <MetaTags>
+                    <title>Number game</title>
+                    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+                </MetaTags>
                 <Main />
             </Provider>
         </ThemeProvider>
