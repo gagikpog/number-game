0388996b718fe8ab13c0dd324b0ee827b9d0dc13
Gagik <gagikpog@gmail.com>
2024-06-01 20:42:00 +0500

    change favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,10 @@ const Index: NextPage = () => {
                 <MetaTags>
                     <title>Number game</title>
                     <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+                    <link rel="mask-icon" href="/favicon.svg" />
+                    <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+                    <link rel="apple-touch-icon" sizes="256x256" href="/favicon.png" />
+                    <link rel="icon" type="image/png" sizes="256x256" href="/favicon.png" />
                 </MetaTags>
                 <Main />
             </Provider>
